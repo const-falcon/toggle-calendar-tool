48ff55058e73ce3d113d69bbe78e2844aa271c36
g-auctioneer <93477932+g-auctioneer@users.noreply.github.com>
2023-10-24 00:22:46 +0900

    ✨ “Add function to toggle checkbox.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,34 @@
 // アイコンがclickされたら発火する
-chrome.action.onClicked.addListener(async () => {
-  await isGoogleCalendarOpen();
+chrome.action.onClicked.addListener(async ({ id }) => {
+  if (!await isGoogleCalendarOpen()) {
+    return;
+  }
+  await chrome.scripting
+    .executeScript({
+      target: { tabId: id },
+      func: toggleCheckboxes,
+    });
 });
 
 // Googleカレンダーを開いているか判定する
 const isGoogleCalendarOpen = async () => {
-  const queryInfo = { active: true, currentWindow: true };
-  const tab = await chrome.tabs.query(queryInfo);
+  const tab = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  });
   const url = tab[0].url;
   return url.indexOf("calendar.google.com") !== -1
+}
+
+// チェックボックスのON/OFF
+const toggleCheckboxes = async () => {
+  const outer = document.getElementById("tkQpTb");
+  const checkboxes = outer.querySelectorAll("input[type='checkbox']");
+  for (const checkbox of checkboxes) {
+    const label = checkbox.getAttribute("aria-label").trim();
+    if (label === "日本の祝日") {
+      continue;
+    }
+    checkbox.checked = !checkbox.checked
+  }
 }
